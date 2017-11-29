2c6e38b143b001303479c6bc904210e7415c0dae
Gabriel Nakajima An <gabriel.nakajima.an@gmail.com>
2017-11-29 01:03:30 -0500

    added docs + minor param changes

--- ddp.py
+++ ddp.py
@@ -36,10 +36,28 @@ def get_lagrangians(lagr, dt):
     return L, Lx, Lu, Lxx, Lxu, Luu
 
 
-def ddp(x_init, m, phi, lagr, F, dt, T=1000, max_iters=100, epsilon=None,
+def ddp(x_init, n, m, phi, lagr, F, dt, T=1000, max_iters=100, epsilon=0.0,
         u_bar=None):
+    """Runs Differential Dynamic Programming (DDP)
+
+    :param x_init: initial state (np.array)
+    :param n: dimension of state
+    :param m: dimension of control
+    :param phi: terminal cost function
+    :param lagr: running cost function
+    :param F: dynamics function F(state, control)
+    :param dt: time step
+    :param T: time horizon
+    :param max_iters: maximum number of iterations
+    :param epsilon: cost error to terminate algorithm
+    :param u_bar: initial nominal control sequence (if None, random sequence
+                  will be used)
+    :return: tuple<optimal control sequence, list<costs of one iteration>>
+    """
+
     # Initialize variables
-    n = len(x_init)
+    if len(x_init) != n:
+        raise ValueError('initial state does not agree with state dimension n')
     u_bar = np.random.rand(T, m) if u_bar is None else u_bar
     x_bar = None
     u_deltas = None
@@ -49,13 +67,14 @@ def ddp(x_init, m, phi, lagr, F, dt, T=1000, max_iters=100, epsilon=None,
     Fx = grad(F, 0)
     Fu = grad(F, 1)
 
+    # Dynamics Linearization and Discretization
     Phi = lambda x: np.eye(n) + Fx(x) * dt
     Beta = lambda x: Fu(x) * dt
 
     iter = 0
     cost = sys.maxsize
     costs = []
-    while (epsilon is None or cost > epsilon) and iter < max_iters:
+    while cost > epsilon and iter < max_iters:
         x_bar = forward_pass(x_init, x_bar, u_bar, u_deltas, F, T, dt)
         u_deltas = []
 
@@ -81,7 +100,7 @@ def ddp(x_init, m, phi, lagr, F, dt, T=1000, max_iters=100, epsilon=None,
             Qxu = Lxu(x) + np.dot(Phi_x.T, Vxx, Beta_x)
             Quu = Luu(x) + np.dot(Beta_x.T, Vxx, Beta_x)
 
-            # Compute optimal change and update nominal control sequence
+            # Compute optimal change
             Quu_inv = np.linalg.inv(Quu)
             u_delta = lambda x_delta: - np.dot(Quu_inv,
                                                Qu + np.dot(Qxu.T, x_delta))
